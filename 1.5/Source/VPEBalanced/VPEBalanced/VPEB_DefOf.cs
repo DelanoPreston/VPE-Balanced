1d4dc589b889925a0d8a2548330a02e156a97e38
DelanoPreston <delanopreston@gmail.com>
2024-07-22 16:23:16 -0600

    removing some of the cs code, reworking hediff for mind break

---
+++ 1.5/Source/VPEBalanced/VPEBalanced/VPEB_DefOf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+[DefOf]
+public static class VPEB_DefOf
+{
+    public static HediffDef VPEB_DominatedMind;
+}
